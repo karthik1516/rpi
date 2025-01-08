7a6120c0a954cc885c06683f42cf731650b23429
Karthik Sankar <55330867+foodalot@users.noreply.github.com>
2025-01-08 18:52:37 -0500

    Fix

--- oled/oled2.py
+++ oled/oled2.py
@@ -16,7 +16,7 @@ def clear(display):
 
 
 i2c = busio.I2C(SCL, SDA)
-display = adafruit_ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c, ADDR)
+display = adafruit_ssd1306.SSD1306_I2C(OLED_WIDTH, OLED_HEIGHT, i2c, addr=ADDR)
 
 
 image = Image.new('1', (OLED_WIDTH, OLED_HEIGHT))
